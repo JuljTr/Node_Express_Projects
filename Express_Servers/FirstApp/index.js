1a65a6c91086a622898c4ec5aff032cacf52f0d7
Juljeta Troka <juliet.troka@gmail.com>
2024-02-05 17:06:32 +0000

    added variable in the route path

--- Express_Servers/FirstApp/index.js
+++ Express_Servers/FirstApp/index.js
@@ -8,17 +8,22 @@ const app = express();
 app.get('/', (req, res) => {
     res.send("HOME PAGE REQUEST")
 })
+app.get('/r/:subreddit', (req, res) => {
+    console.log(req.params);
+    const { subreddit } = req.params;
+    res.send(`<h1> Browsing the: ${subreddit} </h1>`);
+})
 app.get('/info', (req, res) => {
-    res.send("INFORMATION PAGE REQUEST")
+    res.send("INFORMATION PAGE REQUEST");
 })
 app.get('/contact', (req, res) => {
-    res.send("CONTACT PAGE REQUEST")
+    res.send("CONTACT PAGE REQUEST");
 })
 app.get('*', (req, res) => {
-    res.send("ERROR PATH NOT KNOW")
+    res.send("ERROR PATH NOT KNOW");
 })
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000")
+    console.log("Listening on port 3000");
 })
 
