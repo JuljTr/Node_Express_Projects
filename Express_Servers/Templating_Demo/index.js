6a926ba6fe33b0171504939ed103862a0ff70e2f
Juljeta Troka <juliet.troka@gmail.com>
2024-02-06 20:45:20 +0000

    created a more complex subreddit demo with EJS

--- Express_Servers/Templating_Demo/index.js
+++ Express_Servers/Templating_Demo/index.js
@@ -1,25 +1,38 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
 })
 
+app.get('/colors', (req, res) => {
+    const colors = [
+        'Yellow', 'Red', 'Purple', 'Green', 'Blue', 'Gray'
+    ]
+    res.render('colors', { colors })
+})
+
 app.get('/r/:subreddit', (req, res) => {
     console.log(req.params)
     const { subreddit } = req.params;
-    res.render('subreddit', { subreddit});
+    const data = redditData[subreddit]
+    if (data) {
+        res.render('subreddit', { ...data });
+    } else {
+        res.render('notfound', { subreddit })
+    }
 })
 
 app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random', { rand: num});
+    res.render('random', { rand: num });
 })
 
-app.listen(8080, ()=> {
+app.listen(8080, () => {
     console.log("LISTENING ON PORT 8080");
 })
